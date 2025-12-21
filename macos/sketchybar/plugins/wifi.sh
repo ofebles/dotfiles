70c39f04005c3aafd3665f25336aa88ffcb076d1
Orestes Febles <ofebles@gmail.com>
2025-12-21 21:59:46 +0000

    Adjust sketchybar colors and fix wifi interface

--- macos/sketchybar/plugins/wifi.sh
+++ macos/sketchybar/plugins/wifi.sh
@@ -5,14 +5,14 @@
 
 # Check Ethernet (en0) status
 # Returns "active" if connected
-ETHERNET_STATUS=$(ifconfig en0 | grep "status: active")
+ETHERNET_STATUS=$(ifconfig en10 | grep "status: active")
 
 if [ -n "$ETHERNET_STATUS" ]; then
   # Ethernet is active
   sketchybar --set $NAME label="Ethernet" icon=󰈀
 else
   # Check Wi-Fi (en1)
-  SSID=$(networksetup -getairportnetwork en1 | cut -d: -f2 | xargs)
+  SSID=$(networksetup -getairportnetwork en0 | cut -d: -f2 | xargs)
   
   if [ "$SSID" = "You are not associated with an AirPort network." ] || [ "$SSID" = "Wi-Fi power is off" ] || [ -z "$SSID" ]; then
      sketchybar --set $NAME label="Disconnected" icon=󰤮
